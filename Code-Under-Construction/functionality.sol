f206590eb6f180a641b1fd19abdb8b173c599d22
VaibhavAgarwalVA <vaiagarwal96@gmail.com>
2017-03-22 23:27:01 +0530

    completed search (exact and range)

--- Code-Under-Construction/functionality.sol
+++ Code-Under-Construction/functionality.sol
@@ -92,100 +92,17 @@ contract functionality{
         else 
             return 2;
     }
-    
-    function range_search( string lat1, string long1, string lat2, string long2) constant returns (string result){
-        uint[2] memory search_lat1 = cordinate_convert(lat1);
-        uint[2] memory search_long1 = cordinate_convert(long1);
-        uint[2] memory search_lat2 = cordinate_convert(lat2);
-        uint[2] memory search_long2 = cordinate_convert(long2);
+
+    function binary_search_back(string lat1) constant returns (uint i){
+        uint[2] memory search_lat = cordinate_convert(lat1);
 
         uint start = 0;
         uint end = data.length-1;
-        
         uint mid;
-        uint i; 
+        uint status;
         uint[2] memory elem_lat;
         uint[2] memory new_elem;
-        uint status;
-        
-        while(start<end && start<data.length && end>=0){
-            mid = (start+end)/2;
-            elem_lat = cordinate_convert(data[mid].latitude);
-            
-            status = compare_cordinates(elem_lat,search_lat1);
-            if(status == 1){
-                end = mid-1;
-            }
-            else if(status == 2){
-                start = mid+1;
-            }
-            else{
-                break;
-            }
-        }
 
-        for(i=mid-1; i>=0; i--){
-            new_elem = cordinate_convert(data[i].latitude);
-            if(compare_cordinates(new_elem,elem_lat)!=0)
-                break;
-        }
-
-        i++;
-        uint starting = i;
-
-        start = i;
-        end = data.length - 1;
-
-        while(start<end && start<data.length && end>=0){
-            mid = (start+end)/2;
-            elem_lat = cordinate_convert(data[mid].latitude);
-            
-            status = compare_cordinates(elem_lat,search_lat2);
-            if(status == 1){
-                end = mid-1;
-            }
-            else if(status == 2){
-                start = mid+1;
-            }
-            else{
-                break;
-            }
-        }
-
-        for(i=mid+1; i<data.length; i++){
-            new_elem = cordinate_convert(data[i].latitude);
-            if(compare_cordinates(new_elem,elem_lat)!=0)
-                break;
-        }
-
-        i--;
-        uint ending = i;
-
-        for(uint j=starting; j<ending; j++){
-            new_elem = cordinate_convert(data[j].longitude);
-            
-            if(compare_cordinates(new_elem,search_long1)==1){
-                if(compare_cordinates(new_elem,search_long2)==2)
-                  result = strConcat(result," ",data[j].hash);
-            }   
-        }
-
-        return result;
-    }
-
-    function direct_search( string lat, string long) constant returns (string result){
-        uint[2] memory search_lat = cordinate_convert(lat);
-        uint[2] memory search_long = cordinate_convert(long);
-        
-        uint start = 0;
-        uint end = data.length-1;
-        
-        uint mid;
-        uint i; 
-        uint[2] memory elem_lat;
-        uint[2] memory new_elem;
-        uint status;
-        
         while(start<end && start<data.length && end>=0){
             mid = (start+end)/2;
             elem_lat = cordinate_convert(data[mid].latitude);
@@ -207,11 +124,76 @@ contract functionality{
             if(compare_cordinates(new_elem,elem_lat)!=0)
                 break;
         }
-        i++;
 
+        i++;
+        return i;
+    }
+
+    function binary_search_forward(string lat1) constant returns (uint i){
+        uint[2] memory search_lat = cordinate_convert(lat1);
+
+        uint start = 0;
+        uint end = data.length-1;
+        uint mid;
+        uint status;
+        uint[2] memory elem_lat;
+        uint[2] memory new_elem;
+
+        while(start<end && start<data.length && end>=0){
+            mid = (start+end)/2;
+            elem_lat = cordinate_convert(data[mid].latitude);
+            
+            status = compare_cordinates(elem_lat,search_lat);
+            if(status == 1){
+                end = mid-1;
+            }
+            else if(status == 2){
+                start = mid+1;
+            }
+            else{
+                break;
+            }
+        }
+
+        for(i=mid+1; i<data.length; i++){
+            new_elem = cordinate_convert(data[i].latitude);
+            if(compare_cordinates(new_elem,elem_lat)!=0)
+                break;
+        }
+
+        i--;
+        return i;
+    }
+    
+    function range_search( string lat1, string long1, string lat2, string long2) constant returns (string result){
+        uint[2] memory search_long1 = cordinate_convert(long1);
+        uint[2] memory search_long2 = cordinate_convert(long2);
+
+        uint starting = binary_search_back(lat1);
+        uint ending = binary_search_forward(lat2);
+
+        uint[2] memory new_elem;
+        for(uint j=starting; j<=ending; j++){
+            new_elem = cordinate_convert(data[j].longitude);
+            
+            if(compare_cordinates(new_elem,search_long1)==1){
+                if(compare_cordinates(new_elem,search_long2)==2)
+                  result = strConcat(result," ",data[j].hash);
+            }   
+        }
+        return result;
+    }
+
+    function direct_search( string lat, string long) constant returns (string result){
+        uint[2] memory search_lat = cordinate_convert(lat);
+        uint[2] memory search_long = cordinate_convert(long);
+
+        uint i = binary_search_back(lat);
+
+        uint[2] memory new_elem;
         for(uint j=i; j<data.length; j++){
             new_elem = cordinate_convert(data[j].latitude);
-            if(compare_cordinates(new_elem,elem_lat)!=0)
+            if(compare_cordinates(new_elem,search_lat)!=0)
                 break;
 
             new_elem = cordinate_convert(data[j].longitude);
