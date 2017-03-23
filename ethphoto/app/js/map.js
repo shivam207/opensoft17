7cf9c763815b42752f620723791ac426b4fc534a
Shivam <agarwal.shivam207@gmail.com>
2017-03-23 22:25:42 +0530

    Markers Corrected. Tag Button Added. Search Box Added

--- ethphoto/app/js/map.js
+++ ethphoto/app/js/map.js
@@ -102,7 +102,7 @@ function initMap() {
     // console.log(NECorner.lng());
     // console.log(SWCorner.lng());
      setScreenPoints(NECorner.lat(),NECorner.lng(),SWCorner.lat(),SWCorner.lng());
-});
+  });
 
     var locations = [
         {lat: -31.563910, lng: 147.154312},
@@ -132,58 +132,134 @@ function initMap() {
 
 
       // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          console.log('geolocation')
-          // console.log(navigator.geolocation)
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            console.log("Hello form map init " + pos.lat + pos.lng)
-            // setLatLang(pos.lat,pos.lng);
-          //  var marker = new google.maps.Marker({
-          // position: pos,
-          // map: map,
-          // // icon: {
-          // //   path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-          // //   scale: 4
-          // //   },
-          // title: 'User Location'
-          // });
-            // console.log()
-            map.setCenter(pos);
-          });
-        }
+    if (navigator.geolocation) {
+      console.log('geolocation')
+      // console.log(navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        console.log("Hello form map init " + pos.lat + pos.lng)
+        // setLatLang(pos.lat,pos.lng);
+      //  var marker = new google.maps.Marker({
+      // position: pos,
+      // map: map,
+      // // icon: {
+      // //   path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+      // //   scale: 4
+      // //   },
+      // title: 'User Location'
+      // });
+        // console.log()
+        map.setCenter(pos);
+      });
+    }
 
     setMarkers(map,locations);
+    searchBox(map);
+}
+
+function searchBox(map){
+
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
 }
 
 function setMarkers(map,locations) {
     
-    console.log("In Markers")
-    var markers=[];
-    
-    for (var i = 0; i < locations.length; i++) {
-        var location = locations[i];  
+  console.log("In Markers")
+  var markers=[];
+  
+  var infowindow = new google.maps.InfoWindow();
+  for (var i = 0; i < locations.length; i++) {
+    var location = locations[i];  
 
     var marker = new google.maps.Marker({
       position: {lat: location.lat, lng: location.lng},
       map: map
     });
 
-    marker.addListener('click',function(){
-                var x = marker.getPosition().lat();
-                var y = marker.getPosition().lng();
-                console.log(x,y,'getImage');
-            });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          var x = marker.getPosition().lat();
+          var y = marker.getPosition().lng();
+          console.log(x,y,'getImage');
+          infowindow.setContent("Get Username from Backend");
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+
+  
+  markers.push(marker);
     
-    markers.push(marker);
   }
 
-
-    var markerCluster = new MarkerClusterer(map, markers,
-        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+  var markerCluster = new MarkerClusterer(map, markers,
+      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
 }
 
+function tagfun(){
+  var t;
+  if(document.getElementById("a").checked) t="a";
+  if(document.getElementById("b").checked) t="b";
+  if(document.getElementById("c").checked) t="c";
+  console.log(t)
+}
