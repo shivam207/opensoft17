a77ca0f73b9b8e0715a4f0b803ad96a2469ec7b3
a7b23 <abhishek.sinha94@gmail.com>
2017-03-23 16:30:22 +0530

    Merging changes

--- ethphoto/app/contracts/simple_storage.sol
+++ ethphoto/app/contracts/simple_storage.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.4.7;
 contract ethPhoto {
   uint private noOfImages;
 
+  //used for comparing two values
+  bytes c=new bytes(0);
+  bytes d=new bytes(0);
+
   struct Image{
     string latitude;
     string longitude;
@@ -55,13 +59,29 @@ contract ethPhoto {
     return noOfImages;
   }
 
+  function getImHash() constant returns (string hash1){
+
+    //result=noOfImages;
+    if (noOfImages == 0)hash1 = "2";
+    else hash1 = _images[noOfImages-1].hash;
+  
+  }
+
+  function getImLong() constant returns (string long){
+
+    //result=noOfImages;
+    if (noOfImages == 0)long = "3";
+    else long = _images[0].longitude;
+  
+  }
+
 
   function saveImage(string hash1,uint tag1 ){
     //Image tmp(hash1, tag1, tmp, tmp2);
    // _images[noOfImages]=Image(hash1,tag1,tmp,tmp2);
     _images[noOfImages].hash=hash1;
     _images[noOfImages].tag=tag1;
-    _images[noOfImages].userid=msg.sender;
+    //_images[noOfImages].userid=msg.sender;
    // _images[noOfImages].latitude=tmp;
    // _images[noOfImages].longitude=tmp2;
     //noOfImages++;
@@ -125,6 +145,113 @@ contract ethPhoto {
     }
   }
 
+
+  function stringToUint(bytes memory b) constant returns (uint result) {
+       // bytes memory b=bytes(s);
+        uint i;
+        result = 0;
+        for (i = 0; i < b.length; i++) {
+            uint c = uint(b[i]);
+            if (c >= 48 && c <= 57) {
+                result = result * 10 + (c - 48);
+            }
+        }
+    }
+
+// compare string values
+  function compareValues(string _a,string _b) constant returns (bool ){
+    bytes memory a = bytes(_a);
+    bytes memory b = bytes(_b);
+
+    uint l1=a.length;
+    uint l2=b.length;
+
+    c=new bytes(0);
+    d=new bytes(0);
+    
+
+    if(a[0]=='-' && b[0]!='-')return true;
+    if(a[0]!='-' && b[0]=='-')return false;
+
+    uint _i=0;
+    uint i;
+
+    uint flag=0;
+
+    if(a[0]=='-'){
+      _i=1;
+    }
+    
+    uint minLength = a.length;
+    if (b.length < minLength) minLength = b.length;
+    for(i=_i;i<minLength;i++){
+      if(a[i]=='.' && b[i]=='.'){
+
+        if(stringToUint(c) < stringToUint(d))return true;
+        if(stringToUint(c) > stringToUint(d))return false;
+        flag=1;
+        continue;
+      }
+      if(a[i]=='.')return true;
+      if(b[i]=='.')return false;      
+      if(flag==1){
+        if(a[i]<b[i])return true;
+        if(a[i]>b[i])return false;
+        continue;
+      }
+      c.push(a[i]);
+      d.push(b[i]);
+    }
+
+    if(flag==1){
+      //return c.length==0;
+      //printf("%d",stringToUint(c));
+      if(stringToUint(c) < stringToUint(d))return true;
+      if(stringToUint(c) > stringToUint(d))return false;
+    }
+    if(a.length < b.length)return true;
+    if(a.length > b.length)return false;
+    if(flag==0){
+      if(stringToUint(c) < stringToUint(d))return true;
+      if(stringToUint(c) > stringToUint(d))return false;
+    }
+   // val=stringToUint(c);
+   
+    return false;
+  }
+
+  function getTotalImages ()constant returns(uint total){
+    //total = 10;
+    total = noOfImages;
+
+  }
+  function browseImageOnMap (string lat1,string long1,string lat2,string long2)constant returns(string _imageHash){
+      uint i=0;
+      uint flag=0;
+      // if(compareValues(lat1,lat2)==false){
+      //   flag=0;
+      // }
+
+      _imageHash=""; //&& compareValues(lat1,_images[i].latitude) && compareValues(_images[i].latitude,lat2) && compareValues(long1,_images[i].longitude) && compareValues(_images[i].longitude,long2);
+      for(i=0;i<noOfImages;i++){
+        _imageHash = _images[0].hash;
+        // if(flag==0 && compareValues(lat1,_images[i].latitude) && compareValues(_images[i].latitude,lat2) && compareValues(long1,_images[i].longitude) && compareValues(_images[i].longitude,long2)){
+        //   if (bytes(_imageHash).length == 0)_imageHash = strConcat("","",_images[i].hash);
+        //   else _imageHash=strConcat(_imageHash," ",_images[i].hash);
+        // }
+        //TODO
+        // if(flag==1 && compareValues(lat2,_images[i].latitude) && compareValues(_images[i].latitude,lat1) && compareValues(long2,_images[i].longitude) && compareValues(_images[i].longitude,long1)){
+        //   if (bytes(_imageHash).length == 0)_imageHash = strConcat("","",_images[i].hash);
+        //   else _imageHash=strConcat(_imageHash," ",_images[i].hash);
+        // }
+      
+      }
+      return _imageHash;
+
+  }//*/
+  //function browse image by topics (list of arrays , boundry might be given)
+ // function showMyImages
+  
   /*
   function showMyImages() internal constant returns (string _imageHash,uint[] _imageTag,string[] _imageLat,string[] _imageLong){
     uint size=0;
--- ethphoto/app/js/interface.js
+++ ethphoto/app/js/interface.js
@@ -83,11 +83,29 @@ function setScreenPoints(latne,longne,latsw,longsw){
   lat2 = latsw;
   long2 = longsw;
   console.log(lat1);
-  console.log(long1);
   console.log(lat2);
+  console.log(long1);
   console.log(long2);
   // locations = get_locations(lat1,lat2,long1,long2);
   // setMarkers(locations);
+  ethPhoto.browseImageOnMap(lat2.toString(),long2.toString(),lat1.toString(),long1.toString()).then(function(value1) {
+    console.log(typeof value1);
+    //console.log("image hash "+value1);
+  });
+  ethPhoto.getTotalImages().then(function(value2) {
+    console.log("total "+value2)
+  });
+  ethPhoto.getImage("2.3","4.1").then(function(value1) {
+    console.log("total hash "+value1)
+  });
+
+  ethPhoto.getImHash().then(function(value1) {
+    console.log("Im hash "+value1)
+  });
+
+  ethPhoto.getImLong().then(function(value1) {
+    console.log("long "+value1)
+  });
 }
 
 // function setLatLang(lat,long) {
@@ -175,6 +193,7 @@ function upload() {
                   lng: position.coords.longitude
                 };
                 console.log("hi"+pos.lat)
+                console.log("hi"+pos.lng)
                 x = pos.lat;
                 y = pos.lng;
               });
@@ -191,15 +210,21 @@ function upload() {
             x = lat;
             y = lon;
         }
-        console.log("GPSlat" + x.toString());
-        console.log("GPSlong" + y.toString());
+        
+        //console.log("GPSlat" + x.toString());
+        //console.log("GPSlong" + y.toString());
+        
         EmbarkJS.Storage.uploadFile(input).then(function(hash) {
           console.log(hash)
-          console.log("GPSlat" + x.toString());
-          console.log("GPSlong" + y.toString());
-          setMarkers([{lat:x,lng:y}]);
+          
+          
           ethPhoto.saveImage(hash,category);
           ethPhoto.saveImage1(x.toString(),y.toString());
+          console.log("GPSlat" + x.toString());
+          console.log("GPSlong" + y.toString());
+          //setMarkers([{lat:x,lng:y}]);
+          
         });
         });
+  
 }
