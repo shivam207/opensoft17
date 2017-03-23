939b7ced90c82b33d1ae6ab3504e00923fd9ce26
Manish Mukherjee <manishmukherjee12@gmail.com>
2017-03-24 02:49:41 +0530

    GUI running

--- ethphoto/app/js/map.js
+++ ethphoto/app/js/map.js
@@ -1,3 +1,30 @@
+var locations = [
+        {lat: -31.563910, lng: 147.154312, cat='A2'},
+        {lat: -33.718234, lng: 150.363181, cat='C2'},
+        {lat: -33.727111, lng: 150.371124, cat='A2'},
+        {lat: -33.848588, lng: 151.209834, cat='B2'},
+        {lat: -33.851702, lng: 151.216968, cat='C2'},
+        {lat: -34.671264, lng: 150.863657, cat='A2'},
+        {lat: -35.304724, lng: 148.662905, cat='B2'},
+        {lat: -36.817685, lng: 175.699196, cat='C2'},
+        {lat: -36.828611, lng: 175.790222, cat='A2'},
+        {lat: -37.750000, lng: 145.116667, cat='B2'},
+        {lat: -37.759859, lng: 145.128708, cat='C2'},
+        {lat: -37.765015, lng: 145.133858, cat='A2'},
+        {lat: -37.770104, lng: 145.143299, cat='B2'},
+        {lat: -37.773700, lng: 145.145187, cat='C2'},
+        {lat: -37.774785, lng: 145.137978, cat='A2'},
+        {lat: -37.819616, lng: 144.968119, cat='B2'},
+        {lat: -38.330766, lng: 144.695692, cat='C2'},
+        {lat: -39.927193, lng: 175.053218, cat='A2'},
+        {lat: -41.330162, lng: 174.865694, cat='B2'},
+        {lat: -42.734358, lng: 147.439506, cat='C2'},
+        {lat: -42.734358, lng: 147.501315, cat='A2'},
+        {lat: -42.735258, lng: 147.438000, cat='B2'},
+        {lat: -43.999792, lng: 170.463352, cat='C2'}
+      ];
+
+
 
 var map;
 
@@ -183,4 +210,11 @@ function tagfun(){
   if(document.getElementById("b").checked) t="b";
   if(document.getElementById("c").checked) t="c";
   console.log(t)
+}
+
+
+
+function setUserLocation(){
+	console.log("GHHEHEHE");
+	 
 }
